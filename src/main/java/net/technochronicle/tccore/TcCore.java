1fbc1842466dc1f637ad841c95715d2d499d2f62
LeeQianXi <qianxilee232@163.com>
2026-01-04 10:57:51 +0800

    init project

---
+++ src/main/java/net/technochronicle/tccore/TcCore.java
@@ -0,0 +1,13 @@
+package net.technochronicle.tccore;
+
+import net.neoforged.fml.common.Mod;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(TcCore.MOD_ID)
+public class TcCore {
+
+    public static final String MOD_ID = "tccore";
+    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+}
